a4d125a9ef3e8d931095951ef1f286142c361887
Picoman123 <pierre.wijns@gmail.com>
2024-07-20 01:42:06 +0200

    Custom Block #7

--- src/main/java/net/picoman/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/picoman/tutorialmod/item/ModCreativeModTabs.java
@@ -22,6 +22,8 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
 
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
+
                         pOutput.accept(Items.DIAMOND);
 
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
@@ -31,6 +33,8 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+                        pOutput.accept(ModBlocks.SOUND_BLOCK.get());
                     })
                     .build());
 
