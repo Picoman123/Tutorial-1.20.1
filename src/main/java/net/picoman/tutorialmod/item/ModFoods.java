469b6889fb3c1edc1d4cad9c8036a08849ab6514
Picoman123 <pierre.wijns@gmail.com>
2024-07-20 01:57:13 +0200

    Custom tooltips #10

---
+++ src/main/java/net/picoman/tutorialmod/item/ModFoods.java
@@ -0,0 +1,11 @@
+package net.picoman.tutorialmod.item;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(2).fast()
+            .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 1).build(); //200
+    //c'est en tics et 20 tics/secondes
+}
